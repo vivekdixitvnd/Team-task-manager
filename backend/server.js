1bf64d8cc419b700da8dcc07888c2a6aa1235f7c
Vivek Kumar Dixit <98418912+vivekdixitvnd@users.noreply.github.com>
2026-05-14 15:14:48 +0530

    Fix routing to serve index.html correctly

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use('/api/tasks', require('./routes/taskRoutes'));
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
