5990d4faf0286efcc81cf20abc207fd3dec3b185
Vivek Kumar Dixit <98418912+vivekdixitvnd@users.noreply.github.com>
2026-05-14 14:34:48 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  // baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://team-task-manager-2-ng7p.onrender.com/api'
 });
 
 API.interceptors.request.use((req) => {
